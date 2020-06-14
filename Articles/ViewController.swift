b44004f523a126eff1d81098d7e708f2a77ad1c6
Sanman Borate <sanman.borate@pratititech.com>
2020-06-14 20:31:52 +0530

    Setup view model to fetch data using ArticleManager, callbacks to ViewController

--- Articles/ViewController.swift
+++ Articles/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let articleViewModel = ArticleViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        articleViewModel.delegate = self
+        articleViewModel.fetchArticles()
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+extension ViewController: ArticleViewModelDelegate {
+    func didFetchArticleAt(page: Int, count: Int) {
+        print("page : \(page) count : \(count)")
+    }
+    
+    func didFailWith(error: String) {
+        print("error : \(error)")
+    }
+    
+}
--- Articles/ViewModels/ArticleViewModel.swift
+++ Articles/ViewModels/ArticleViewModel.swift
@@ -8,7 +8,15 @@
 
 import Foundation
 
+protocol ArticleViewModelDelegate  {
+    func didFetchArticleAt(page :Int, count:Int)
+    func didFailWith(error: String)
+}
+
 class ArticleViewModel {
+    
+    let articleManager = ArticleManager()
+    var delegate : ArticleViewModelDelegate?
     
     var canGoToNextPage : Bool = true
     var currentPage = 1
@@ -16,8 +24,33 @@ class ArticleViewModel {
     var fetchedData : [Article] = []
     var cellViewModels : [ArticleCellViewModel] = []
     
-    func fetchNextPage() {
-        currentPage = currentPage + 1
+    init() {
+        articleManager.delegate = self
     }
     
+    func fetchNextPage() {
+        currentPage = currentPage + 1
+        fetchArticles()
+    }
+    
+    func fetchArticles() {
+        articleManager.getArticlesAt(page: currentPage, limit: limit)
+    }
+}
+
+extension ArticleViewModel : ArticleManagerDelegate {
+    func didFetchaArticles(articles: [Article], at page: Int) {
+        fetchedData.append(contentsOf: articles)
+        let cellVMs = articles.map { (article) -> ArticleCellViewModel in
+            ArticleCellViewModel(article: article)
+        }
+        cellViewModels.append(contentsOf: cellVMs)
+        delegate?.didFetchArticleAt(page: page, count: articles.count)
+    }
+    
+    func didFailToFetchArticle(at page: Int, error: Error?) {
+        if let error = error as? ArticleManagerError, error == ArticleManagerError.noData {
+            delegate?.didFailWith(error: "No more data")
+        }
+    }
 }
